99bbd27ef780cd2a111ddc8f82a45c92450931ba
JustOneDeveloper <78445321+Just1Developer@users.noreply.github.com>
2025-01-24 23:43:14 +0100

    Refactor Shortlinks

--- src/lib/findshortlink.ts
+++ src/lib/findshortlink.ts
@@ -1,15 +1,11 @@
 "use server"
 
+const LINK_REFS = {
+    "github":               "https://github.com/Just1Developer/",
+    "template":             "https://github.com/Just1Developer/shortlinks-template/",
+    "exma":                 "https://www.sle.kit.edu/downloads/Sonstige/Exmatrikulation_WEB_Formular.pdf",
+} as Record<string, string>;
+
 export const getLink = async ({ shortlink }: { shortlink: string })=> {
-    switch (shortlink) {
-        // Some examples:
-        case "github":
-            return "https://github.com/Just1Developer/"
-        case "template":
-            return "https://github.com/Just1Developer/shortlinks-template/"
-        case "exma":    // For the meme
-            return "https://www.sle.kit.edu/downloads/Sonstige/Exmatrikulation_WEB_Formular.pdf"
-        default:
-            return "/"  // Defaults to error page
-    }
+    return LINK_REFS[shortlink] ?? "/";
 }
